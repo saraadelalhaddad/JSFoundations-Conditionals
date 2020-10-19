def1c29c729a1c69601331ae3dbaa90127333334
saraadelalhaddad <sara.alhaddad@colorado.edu>
2020-10-19 17:53:48 +0300

    Solved the Conditionals task.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
 // Define the numbers and the operation here
+const firstnumber = 5;
+
+const secondnumber = 7; 
+
+const operation = "*";
+
 
 // Write if-statements here
+
+if (operation === "+") {
+    console.log(`${firstnumber} + ${secondnumber} = ${firstnumber + secondnumber}`);
+} else if (operation === "*") {
+    console.log(`${firstnumber} * ${secondnumber} = ${firstnumber * secondnumber}`);
+} else if (operation === "-") {
+    console.log(`${firstnumber} - ${secondnumber} = ${firstnumber - secondnumber}`);
+} else if (operation === "/") {
+    console.log(`${firstnumber} / ${secondnumber} = ${firstnumber / secondnumber}`);
+}
